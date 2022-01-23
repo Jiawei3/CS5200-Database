b791f2a1c5bdfc0da209ce7c5a7ddfa533eee497
Jiawei Liu <jiaweiliu1998@gmail.com>
2022-01-22 21:20:18 -0800

    update

--- db_design_library.sql
+++ db_design_library.sql
@@ -23,10 +23,10 @@ DROP TABLE IF EXISTS `library`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `library` (
-  `Author` text,
-  `Publish_date` text,
-  `Book_name` text,
-  `Publisher` text
+  `Author` varchar(100) DEFAULT NULL,
+  `Publish_date` date DEFAULT NULL,
+  `Book_name` varchar(100) DEFAULT NULL,
+  `Publisher` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -36,13 +36,9 @@ CREATE TABLE `library` (
 
 LOCK TABLES `library` WRITE;
 /*!40000 ALTER TABLE `library` DISABLE KEYS */;
-INSERT INTO `library` VALUES ('Andrew Jacobson','(2003 April 27)','“Blue Gift”','CRC Press'),('Bill Kauffmann','(2005 March 6)','“Willow in the Mist”','Delirium Books'),('Bill Kern','(2005 March 1)','“Time of Silence”','Del Sol Press'),('Catherine Wilde','(2008 June 30)','“Black Tears”','CRC Press'),('John Becker','(2008 May 2)','“Splintered History”','ABC Publishing'),('John Becker','(2006 July 17)','“Lost Ashes”','Delirium Books'),('John Becker','(2010 January 15)','“Forgotten Girl”','ABC Publishing'),('Michael Ernst','(1954 December 4)','“Tower in the Flame”','Beyer Books'),('Natalie Haast','(1988 February 25)','“Frozen Legacy”','Beyer Books'),('Samantha Steele','(1999 October 8)','“Doors of Danger”','Beyer BABC Publishingoks'),('Samantha Steele','(1993 February 11)','“Diamond Petals”','Beyer Books');
+INSERT INTO `library` VALUES ('Andrew Jacobson','2003-04-27','Blue Gift','CRC Press'),('Bill Kauffmann','2005-03-06','Willow in the Mist','Delirium Books'),('Bill Kern','2005-03-01','Time of Silence','Del Sol Press'),('Catherine Wilde','2008-06-30','Black Tears','CRC Press'),('John Becker','2008-05-02','Splintered History','ABC Publishing'),('John Becker','2006-07-17','Lost Ashes','Delirium Books'),('John Becker','2010-01-15','Forgotten Girl','ABC Publishing'),('Michael Ernst','1954-12-04','Tower in the Flame','Beyer Books'),('Natalie Haast','1988-02-25','Frozen Legacy','Beyer Books'),('Samantha Steele','1999-10-08','Doors of Danger','Beyer BABC Publishingoks'),('Samantha Steele','1993-02-11','Diamond Petals','Beyer Books');
 /*!40000 ALTER TABLE `library` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Dumping routines for database 'db_design'
---
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -53,4 +49,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-01-22 17:43:30
+-- Dump completed on 2022-01-22 21:15:34
