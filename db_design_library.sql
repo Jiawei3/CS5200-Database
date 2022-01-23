9a2ef98ac547715c62dfa802480881d8bfdf221e
Jiawei Liu <jiaweiliu1998@gmail.com>
2022-01-22 17:44:01 -0800

    change

--- db_design_library.sql
+++ db_design_library.sql
@@ -1,8 +1,56 @@
-CREATE DATABASE db_design;
-USE db_design;
-CREATE TABLE library(Author VARCHAR(70),
-					 Publish_data VARCHAR(70),
-                     Book_name VARCHAR(70),
-                     Publisher VARCHAR(70));
-SELECT * FROM library;
+-- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
+--
+-- Host: localhost    Database: db_design
+-- ------------------------------------------------------
+-- Server version	8.0.20
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `library`
+--
+
+DROP TABLE IF EXISTS `library`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `library` (
+  `Author` text,
+  `Publish_date` text,
+  `Book_name` text,
+  `Publisher` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `library`
+--
+
+LOCK TABLES `library` WRITE;
+/*!40000 ALTER TABLE `library` DISABLE KEYS */;
+INSERT INTO `library` VALUES ('Andrew Jacobson','(2003 April 27)','“Blue Gift”','CRC Press'),('Bill Kauffmann','(2005 March 6)','“Willow in the Mist”','Delirium Books'),('Bill Kern','(2005 March 1)','“Time of Silence”','Del Sol Press'),('Catherine Wilde','(2008 June 30)','“Black Tears”','CRC Press'),('John Becker','(2008 May 2)','“Splintered History”','ABC Publishing'),('John Becker','(2006 July 17)','“Lost Ashes”','Delirium Books'),('John Becker','(2010 January 15)','“Forgotten Girl”','ABC Publishing'),('Michael Ernst','(1954 December 4)','“Tower in the Flame”','Beyer Books'),('Natalie Haast','(1988 February 25)','“Frozen Legacy”','Beyer Books'),('Samantha Steele','(1999 October 8)','“Doors of Danger”','Beyer BABC Publishingoks'),('Samantha Steele','(1993 February 11)','“Diamond Petals”','Beyer Books');
+/*!40000 ALTER TABLE `library` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'db_design'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-01-22 17:43:30
