9040c9d46cc948e91d34189733406a4464b8e3b9
Jiawei Liu <jiaweiliu1998@gmail.com>
2022-01-22 17:39:15 -0800

    A1

---
+++ db_design_library.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE db_design;
+USE db_design;
+CREATE TABLE library(Author VARCHAR(70),
+					 Publish_data VARCHAR(70),
+                     Book_name VARCHAR(70),
+                     Publisher VARCHAR(70));
+SELECT * FROM library;
+
